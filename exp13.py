e5e876b416acd923a654a7880bc48059d5a48fcf
Santosh <santosh.m06@gmail.com>
2019-03-13 15:18:08 +0530

    added 12, 13, 14, 15 exp

---
+++ exp13.py
@@ -0,0 +1,10 @@
+#! /usr/bin/python3.5
+
+from sys import argv
+
+script, first, second, third = argv
+
+print("The script is called:", script)
+print("Your first variable:", first)
+print("Your second variable:", second)
+print("Your third variable:", third)
